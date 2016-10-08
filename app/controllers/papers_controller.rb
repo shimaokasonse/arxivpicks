b2c6438eb95f5aea0474c034b1b97d4d2c86f197
shimaokasonse <simaokasonse@yahoo.co.jp>
2016-10-09 08:48:10 +0900

    アクションindexでの記事のコメント数によるランキング機能を実装

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,17 +1,18 @@
 class PapersController < ApplicationController
 
   def index
-    @papers = Paper.all.order('id ASC').page(params[:page]).per(5)
+    ids = Comment.group(:paper_id).order('count_paper_id DESC')
+                 .page(params[:page]).per(5).count(:paper_id).keys
+    @papers = ids.map{ |id| Paper.find(id)}
   end
 
   def search
-    @papers = Paper.where("title LIKE(?)","%#{params[:keyword]}%").limit(20)
+    @papers = Paper.where("title LIKE(?)","%#{params[:keyword]}%").limit(5).order('date DESC')
   end
 
   def show
     @paper = Paper.find(params[:id])
     @comments = @paper.comments
   end
-
 
 end
