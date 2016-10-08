e9da5d5f90b9e50b17eb4c54687b4f9379487983
shimaokasonse <simaokasonse@yahoo.co.jp>
2016-10-08 21:31:32 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'papers#index'
+
   resources :papers, only: :show do
     resources :comments, only: [:new,:create,:edit,:update,:destroy] do
       resources :likes, only: [:create,:destroy]
     end
-
     collection do
       get 'search'
     end
-  end  
-  root 'papers#index'
+  end
+
+  resources :users, only: :show
+  
 end
