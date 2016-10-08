af14951b840c26d926f8c87d9e49aa3341c10326
shimaokasonse <simaokasonse@yahoo.co.jp>
2016-10-09 07:24:41 +0900

    Like(いいね！)機能を実装

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to  :user
   belongs_to :paper
   has_many :likes
+
+  def like_count
+    likes.count
+  end
+
+  def like_user(user_id)
+   likes.find_by(user_id: user_id)
+  end
+  
 end
