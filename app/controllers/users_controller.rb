0e1ef4a5406d7c10863bfc49d8aac1d729a2b653
shimaokasonse <simaokasonse@yahoo.co.jp>
2016-10-09 23:07:16 +0900

    ユーザーページを実装(一旦showのみ)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
   def show
+    user = User.find(params[:id])
+    @name = user.name
+    @introduction = user.introduction
+    @comments = Comment.where(user_id: params[:id]).order("created_at ASC")
   end
 
 end
